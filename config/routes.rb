bd7e57adc02b58c55e2a917d0a7e856b0211021f
Francisco Mancilla <frmancil@hotmail.com>
2024-03-14 21:31:08 -0500

    about page and conditional for missing images on pokemon index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/', to: 'pokemons#index'
   get '/abilities', to: 'abilities#index'
   get '/types', to: 'types#index'
+  get '/about', to: 'about#show'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   resources :pokemons
   resources :abilities
   resources :types
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
